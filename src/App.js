937cb6f16082eaa5e2180e626b3d3555ac540804
reddvil <zurab.okroshidze.1@btu.edu.ge>
2021-06-04 00:01:14 +0400

    Destructure Routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import { Route, Switch } from 'react-router';
+import Navigation from './components/navigation';
+import Routes from './Routes'
 import './App.css';
-import Auth from './pages/auth';
-import Home from './pages/home';
-import Navigation from './components/navigation'
-import Greeting from './pages/greetingPage'
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      <Switch>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/auth">
-          <Auth />
-        </Route>
-
-        <Route path="/">
-          <Greeting />
-        </Route>
-
-      </Switch>
+      <Navigation />
+      <Routes />
     </div>
   );
 }
